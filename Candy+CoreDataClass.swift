18dab4b26bd3c9bbd3db73f1084607fa850d2b5d
cesarlopezt <cesarlopez2808@gmail.com>
2023-04-01 14:09:25 -0500

    One-to-many relationships core data

---
+++ Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Cesar Lopez on 4/1/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Cesar Lopez on 4/1/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
